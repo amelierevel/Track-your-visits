edb98ca397592c115ecf660d3cada7db5aa10d89
Amélie Revel <amelie.revel20@yahoo.fr>
2018-10-29 10:05:55 +0100

    ajout htaccess et modif footer

---
+++ error404.php
@@ -0,0 +1,7 @@
+<?php include_once 'header.php'; ?>
+<div class="bgText container">
+    <h2 class="center-align">ERREUR 404</h2>
+    <p class="center-align">Page non trouvée</p>
+</div>
+<?php include 'footer.php'; ?>
+
--- footer.php
+++ footer.php
@@ -6,18 +6,19 @@
                         <p class="grey-text text-lighten-4">You can use rows and columns here to organize your footer content.</p>
                     </div>
                     <div class="col l4 offset-l2 s12">
-                        <h5 class="white-text">Links</h5>
+                        <h5 class="white-text">Contact</h5>
                         <ul>
-                            <li><a class="grey-text text-lighten-3" href="#">Link 1</a></li>
-                            <li><a class="grey-text text-lighten-3" href="#">Link 2</a></li>
+                            <li><a class="grey-text text-lighten-3" href="https://github.com/amelierevel" target="_blank"><i class="fab fa-github"></i> Github</a></li>
+                            <li><a class="grey-text text-lighten-3" href="https://www.linkedin.com/in/amelie-revel-387541b8/" target="_blank"><i class="fab fa-linkedin"></i> LinkedIn</a></li>
+                            <li><a class="grey-text text-lighten-3" href="mailto:amelie.revel20@yahoo.fr"><i class="fas fa-envelope"></i> Contacter par mail</a></li>
                         </ul>
                     </div>
                 </div>
             </div>
             <div class="footer-copyright">
                 <div class="container">
-                    © 2018 Copyright Text
-                    <a class="grey-text text-lighten-4 right" href="#">More Links</a>
+                    © 2018 Copyright
+                    <a class="grey-text text-lighten-4 right" href="https://amelierevel.github.io/" target="_blank">Amélie Revel</a>
                 </div>
             </div>
         </footer>
