080e304216b8ec3eb254f8d17e7f32bc67397b42
Amélie Revel <amelie.revel20@yahoo.fr>
2018-12-10 23:28:26 +0100

    correction mineure

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,7 @@
 $(document).ready(function () {
 //--------script pour materialize-------------------
     $('.sidenav').sidenav();    //pour l'affichage du menu de navigation en responsive
-    $(".dropdown-trigger").dropdown();  //pour l'affichage du dropdown de la barre de navigation
+    $('.dropdown-trigger').dropdown();  //pour l'affichage du dropdown de la barre de navigation
     $('select').formSelect();  //pour l'affichage du select du formulaire d'inscription utilisateur
     $('.modal').modal();    //pour l'affichage de la modal pour la connexion dans la barre de navigation
     $('textarea#priceName').characterCounter(); //pour le calcul du nombre de caractères
